359752244f35f33f8567a26e95563be403ae2cba
unibrij <ceo@unibrij.io>
2026-05-06 03:50:10 -0300

    Delete partner-docs/assets/access-guard.js

--- partner-docs/assets/access-guard.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  const hasAccess =
-    sessionStorage.getItem("ub_api_reference_access") === "granted";
-
-  if (!hasAccess) {
-    window.location.replace("/partner-docs/");
-  }
-})();
