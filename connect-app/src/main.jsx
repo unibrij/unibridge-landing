98a33938af96c206e8570c1223f24fb4def01d54
unibrij <ceo@unibrij.io>
2026-05-22 23:21:27 -0300

    Create main.jsx

---
+++ connect-app/src/main.jsx
@@ -0,0 +1,22 @@
+// connect-app/src/main.jsx
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import App from "./App.jsx";
+import { wagmiAdapter } from "./appkit.js";
+import "./App.css";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>
+);
