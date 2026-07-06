873b974f722171e617351f78f5dd242bde0d5a94
unibrij <ceo@unibrij.io>
2026-07-06 17:16:30 -0300

    Create partnerPortalUseCases.js

---
+++ partner-portal/js/partnerPortalUseCases.js
@@ -0,0 +1,44 @@
+// partner-portal/js/partnerPortalUseCases.js
+
+export const PARTNER_USE_CASE_OPTIONS = [
+  [
+    "",
+    "Select use case"
+  ],
+  [
+    "remittances",
+    "Remittances"
+  ],
+  [
+    "cross_border_business_payments",
+    "Cross-border business payments"
+  ],
+  [
+    "merchant_settlement",
+    "Merchant settlement"
+  ],
+  [
+    "marketplace_payouts",
+    "Marketplace / platform payouts"
+  ],
+  [
+    "payroll_contractor_payments",
+    "Payroll / contractor payments"
+  ],
+  [
+    "wallet_cash_out",
+    "Wallet cash-out"
+  ],
+  [
+    "stablecoin_off_ramp",
+    "Stablecoin off-ramp"
+  ],
+  [
+    "corporate_treasury",
+    "Corporate treasury"
+  ],
+  [
+    "other",
+    "Other"
+  ]
+];
