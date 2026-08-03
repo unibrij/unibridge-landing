0df278cb9415b858ddb518c5cfaec9534785ad10
unibrij <ceo@unibrij.io>
2026-08-02 23:00:38 -0300

    Update appkit.js

--- connect-app/src/appkit.js
+++ connect-app/src/appkit.js
@@ -1,34 +1,64 @@
 // connect-app/src/appkit.js
 
-import { createAppKit } from "@reown/appkit/react";
-import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
-import { polygon } from "@reown/appkit/networks";
+import {
+  createAppKit
+} from "@reown/appkit/react";
 
-export const projectId = "c15ae70122dad197db547f7bc77cea37";
+import {
+  WagmiAdapter
+} from "@reown/appkit-adapter-wagmi";
 
-export const networks = [polygon];
+import {
+  polygon
+} from "@reown/appkit/networks";
+
+export const projectId =
+  "c15ae70122dad197db547f7bc77cea37";
+
+export const networks = [
+  polygon
+];
 
 export const metadata = {
-  name: "UniBridge",
-  description: "UniBridge Connect",
-  url: "https://unibrij.io",
-  icons: ["https://unibrij.io/connect/icons/social/Ub.png"]
+  name:
+    "UniBridge Connect",
+
+  description:
+    "Pay verified payout routes with your wallet",
+
+  url:
+    "https://unibrij.io/connect/",
+
+  icons: [
+    "https://unibrij.io/connect/icons/social/Ub.png"
+  ]
 };
 
-export const wagmiAdapter = new WagmiAdapter({
-  networks,
-  projectId,
-  ssr: false
-});
+export const wagmiAdapter =
+  new WagmiAdapter({
+    networks,
+    projectId,
+    ssr:
+      false
+  });
 
 createAppKit({
-  adapters: [wagmiAdapter],
+  adapters: [
+    wagmiAdapter
+  ],
+
   networks,
   projectId,
   metadata,
+
   features: {
-    analytics: false,
-    onramp: false,
-    swaps: false
+    analytics:
+      false,
+
+    onramp:
+      false,
+
+    swaps:
+      false
   }
 });
