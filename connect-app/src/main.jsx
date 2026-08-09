7dbcfd017b4b4ecf9ac1ccaf479e2f23bd08dd03
unibrij <ceo@unibrij.io>
2026-08-08 23:36:55 -0300

    Update main.jsx

--- connect-app/src/main.jsx
+++ connect-app/src/main.jsx
@@ -7,7 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App.jsx";
 import { wagmiAdapter } from "./appkit.js";
-import "./App.css";
+import "./styles/connect.css";
 
 const queryClient = new QueryClient();
 
