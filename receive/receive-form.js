88738302398c7f10810778a17f2b68a05c85fda7
unibrij <ceo@unibrij.io>
2026-08-27 23:17:23 -0300

    Create receive-form.js

---
+++ receive/receive-form.js
@@ -0,0 +1,14 @@
+// unibridge-landing/receive/receive-form.js
+
+export {
+  buildReceiveCatalog,
+  railsForCountry,
+  findReceiveRoute,
+  formatRailLabel
+} from "./receive-catalog.js";
+
+export {
+  renderReceiveFields,
+  collectReceiveBeneficiary,
+  cancelReceiveFieldRender
+} from "./receive-fields.js";
